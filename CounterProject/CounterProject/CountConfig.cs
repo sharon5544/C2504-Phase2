330c2941c89f8ab3665778a321e7a721e0c6fe52
sharon5544 <sharorose20@gmail.com>
2024-10-03 09:14:33 +0530

    Counter Project
    Counter Project using MVVM

---
+++ CounterProject/CounterProject/CountConfig.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CounterProject
+{
+    public class CountConfig
+    {
+        public static CounterViewModel VueModel { get; set; }
+        
+        static CountConfig()
+        { 
+            VueModel = new CounterViewModel(); 
+        }
+    }
+}
---
+++ CounterProject/CounterProject/CounterViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CounterProject
+{
+    public class CounterViewModel : INotifyPropertyChanged
+    {
+        private int _counterValue;
+        public int CounterValue
+        {
+            get { return _counterValue; }
+            set
+            {
+                _counterValue = value;
+                OnPropertyChanged(nameof(CounterValue));
+            }
+        }
+
+        public ICommand IncrementCommand { get; }
+        public ICommand DecrementCommand { get; }
+
+
+        public CounterViewModel()
+        {
+            CounterValue = 0;
+            IncrementCommand = new RelayCommand(IncrementCounter);
+            DecrementCommand = new RelayCommand(DecrementCounter);
+        }
+
+        private void IncrementCounter()
+        {
+            CounterValue++;
+        }
+        private void DecrementCounter()
+        {
+            CounterValue--;
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+}
+
